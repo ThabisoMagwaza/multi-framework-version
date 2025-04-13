96012a8d83642708442e68dd61467e381a63f5a5
Thabiso Magwaza <magwazathabiso9@gmail.com>
2025-04-13 16:58:53 +0200

    fix: trying public path

--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -1,9 +1,19 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
 module.exports = withModuleFederationPlugin({
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
+  },
 
-    shared: {
-        ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-    },
-
+  output: {
+    publicPath:
+      process.env.NODE_ENV === "production" ? "/container/latest/" : "/",
+  },
 });
